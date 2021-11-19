229fd02fddfaefc8296227abb817eebf220ade9a
Mohibul Alom <mohibulalom@outlook.es>
2021-11-19 13:19:31 +0100

    FEAT: added both ways

--- Kata-1/index.js
+++ Kata-1/index.js
@@ -1,59 +1,10 @@
-const INTEGER_ROMAN = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-}
+//RomanNumerals.toRoman(1000); // should return 'M'
 
-const ROMAN_INTEGER = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I",
-}
-
-const toRoman = (num) => {
-    let roman = "";
-
-    Object.keys(INTEGER_ROMAN).forEach(i => {
-        let position = Math.floor(num / INTEGER_ROMAN[i]);
-        num -= position * INTEGER_ROMAN[i];
-        roman += i.repeat(position);
-    })
-
-    return roman;
-}
-
-const fromRoman = (string) => {
-
-}
-
-// console.log(toRoman(1000))
-
-
-function solution(number) {
-    var map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 },
+function toRoman(number) {
+    let map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 },
         output = '';
 
-    for (var i in map) {
+    for (let i in map) {
         while (number >= map[i]) {
             output += i;
             number -= map[i];
@@ -62,4 +13,32 @@ function solution(number) {
     return output;
 }
 
-console.log(solution(57));
+console.log(toRoman(1990));
+
+//RomanNumerals.fromRoman('M'); // should return 1000
+
+const CHAR_INT = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+function fromRoman(roman) {
+    if (roman === null) return -1;
+    let num = CHAR_INT[roman.charAt(0)]
+    let pre, curr;
+
+    for (let i = 1; i < roman.length; i++) {
+        curr = CHAR_INT[roman.charAt(i)];
+        pre = CHAR_INT[roman.charAt(i - 1)]
+        curr <= pre ? num += curr : num = num - pre * 2 + curr;
+    }
+
+    return num;
+}
+
+console.log(fromRoman("LVII"))
